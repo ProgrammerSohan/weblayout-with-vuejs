4f254766a4f5c5b4fc792f3f7b34ee6de92669ec
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-09-21 14:54:23 +0600

    01_some update

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
     <div>
-        <!--  <UserInfovalidation name="Sohan" surname="Sujan" age="1296" /> -->
-          <!-- <UserInfovalidation :name="user.name" :surname="user.surname" :age="user.age" /> -->
+      
            <BooleanBox />
        
     </div>
@@ -9,31 +8,16 @@
 </template>
 <script>
 import BooleanBox from './components/BooleanBox.vue';
-//import UserInfovalidation from './components/UserInfovalidation.vue';
+
 
 
 
 
 export default{
     name: 'App',
-    data(){
-        return {
-           // page: "Hello I am Sohan!!",
-             
-           user:{
-                // name: "Bishop",
-                name: 56,
-                surname: "John",
-                age: 55,
-
-           }
-          
-
-            
-        };
-    },
+   
     components: {
-       // UserInfovalidation,
+      
         BooleanBox
 
     }
